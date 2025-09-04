b01008c6224a61efa1c953dbc972c73906f5b1e7
Daniel Lappe <daniellappe@icloud.com>
2025-09-04 11:35:59 +0200

    Final final changes, added two more books

--- main.py
+++ main.py
@@ -1,25 +1,29 @@
+import sys
 from stats import word_count
 from stats import get_book_text
 from stats import letter_count
 from stats import sorted_letter_count
 
+if len(sys.argv) != 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+
 def main(relative_path):
 
-    print(get_book_text(relative_path))
-    
+    letters_counted = letter_count(get_book_text(relative_path))
 
-letters_counted = letter_count(get_book_text("books/frankenstein.txt"))
+    letters_counted_and_sorted = sorted_letter_count(letters_counted)
 
-letters_counted_and_sorted = sorted_letter_count(letters_counted)
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at " + str(relative_path) + "...")
+    print("----------- Word Count ----------")
+    word_count(relative_path)
+    print("--------- Character Count -------")
+    for obj in letters_counted_and_sorted:
+        print(obj[0] + ": " + str(obj[1]))
+    print("============= END ===============")
 
-print("============ BOOKBOT ============")
-print("Analyzing book found at books/frankenstein.txt...")
-print("----------- Word Count ----------")
-word_count("books/frankenstein.txt")
-print("--------- Character Count -------")
-for obj in letters_counted_and_sorted:
-    print(obj[0] + ": " + str(obj[1]))
-print("============= END ===============")
+main(sys.argv[1])
 
 
 

ec13ebbd2cb5fc3d02b5621a30abd8dd2f9711fb
Daniel Lappe <daniellappe@icloud.com>
2025-09-04 11:13:36 +0200

    Made further changes, final output created

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
 from stats import word_count
 from stats import get_book_text
 from stats import letter_count
+from stats import sorted_letter_count
 
 def main(relative_path):
 
     print(get_book_text(relative_path))
     
-word_count("books/frankenstein.txt")
 
 letters_counted = letter_count(get_book_text("books/frankenstein.txt"))
-print(letters_counted)
+
+letters_counted_and_sorted = sorted_letter_count(letters_counted)
+
+print("============ BOOKBOT ============")
+print("Analyzing book found at books/frankenstein.txt...")
+print("----------- Word Count ----------")
+word_count("books/frankenstein.txt")
+print("--------- Character Count -------")
+for obj in letters_counted_and_sorted:
+    print(obj[0] + ": " + str(obj[1]))
+print("============= END ===============")
 
 
 
--- stats.py
+++ stats.py
@@ -12,7 +12,7 @@ def word_count(filepath):
     book_str = get_book_text(filepath)
     words = book_str.split()
 
-    print("{num_words} words found in the document".format(num_words=len(words)))
+    print("Found {num_words} total words".format(num_words=len(words)))
 
 def letter_count(book_text):
 
@@ -22,3 +22,10 @@ def letter_count(book_text):
             letters_dict[letter.lower()] += 1
     
     return letters_dict
+
+def sorted_letter_count(letter_dict):
+
+    sorted_dict = sorted(letter_dict.items(), key=lambda x: -x[1])
+
+    return sorted_dict
+
